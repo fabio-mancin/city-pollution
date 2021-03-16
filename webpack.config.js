78a30c69f465e8742e05561309f4be1d60c8a6ce
fabio-mancin <fabio.mancin90@gmail.com>
2021-03-16 16:58:22 +0100

    Deployment tests

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,17 @@
 const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = env => {
   const Dotenv = require('dotenv-webpack');
 
   return {
     mode: 'development',
     watch: true,
-    entry: path.resolve(__dirname, 'src') + '/index.js',
+    entry: path.resolve(__dirname, './src') + '/index.js',
     output: {
-      filename: 'index.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, './build'),
     },
     module: {
       rules: [{
@@ -20,7 +23,12 @@ module.exports = env => {
       }],
     },
     plugins: [
-      new Dotenv()
+      new Dotenv(),
+      new webpack.optimize.UglifyJsPlugin(),
+      new HtmlWebpackPlugin({
+        inject: true,
+        template:  path.resolve('./index.html'),
+    }),
     ]
   };
 };
